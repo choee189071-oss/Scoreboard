1c7c47f474c15c6fb0d25a4daa70486f4ebcd108
choee189071-oss <choee189071@gmail.com>
2026-05-27 11:13:57 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,16 @@ criteria_type = st.sidebar.selectbox(
     ]
 )
 st.write(f"Currently Selected: {criteria_type}")
+
+
+import pandas as pd
+st.subheader("Upload Financial Data")
+uploaded_file = st.file_uploader(
+    "Upload CSV File",
+    type=["csv"]
+)
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.subheader("Preview Data")
+    st.dataframe(df)
+    st.success("File uploaded successfully.")
