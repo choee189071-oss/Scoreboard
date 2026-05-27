05e1df1a81d2da07a549ddc3aad43c2927c305dd
choee189071-oss <choee189071@gmail.com>
2026-05-27 11:13:46 -0700

    Update scoring_general_fund.py

--- modules/scoring_general_fund.py
+++ modules/scoring_general_fund.py
@@ -1,11 +1,1 @@
-import pandas as pd
-st.subheader("Upload Financial Data")
-uploaded_file = st.file_uploader(
-    "Upload CSV File",
-    type=["csv"]
-)
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    st.subheader("Preview Data")
-    st.dataframe(df)
-    st.success("File uploaded successfully.")
+
